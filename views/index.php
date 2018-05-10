ab8e9e20ba578e4f7a9e73a7ed24a72b1d36d21a
Alex <arpearce93@gmail.com>
2018-05-10 08:27:24 +0100

    First commit

---
+++ views/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang = "en">
+   
+   <head>
+	    
+	    <?php
+	    include("header.html");
+	    ?>
+	      
+   </head>
+   
+   <body>
+    	<?php
+	    include("navBar.html");
+	    ?>
+
+	
+
+		
+	 
+
+	
+	 
+
+
+      
+
+   </body>
+</html>
