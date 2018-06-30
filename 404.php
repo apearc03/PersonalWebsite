a5d68e2b4dde00317bccfcf57ab6f077f5a5e5eb
Alex <arpearce93@gmail.com>
2018-06-30 16:33:36 +0100

    Added custom 404 page

---
+++ 404.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang = "en">
+   
+   <head>
+	    
+	    <?php
+	     require("static/includes.html");
+	    ?>
+	      
+   </head>
+   
+   <body>
+   
+    <?php
+	    require("static/navBar.html");
+	  ?>
+
+	 
+
+
+		 <div class="container-fluid project">
+		
+		   		    <div class="row">
+			      		<div class="mx-auto">
+							<h2>Error, that page doesn't exist.</h2>
+						</div>
+		     		 </div>
+		</div>
+	  
+	  <?php
+	    require("static/footerBar.html");
+	  ?>
+
+      
+
+   </body>
+</html>
